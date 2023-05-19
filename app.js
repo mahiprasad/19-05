e0f8d4c15ec85b8e9e772015ed26d7a24cc24fea
Mahi Prasad <mp2937@srmist.edu.in>
2023-05-19 19:55:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-//jshint esversion:6
 const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const _ = require("lodash");
-const https = require("https");
 const axios = require("axios");
 
 const app = express();
-dotenv.config();
-app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-//---------------------------------------------------------------
-const url =
-  "https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json";
+// Define a route to retrieve and render the JSON data
+app.get("/", async (req, res) => {
+  try {
+    // Retrieve the JSON data
+    const response = await axios.get(
+      "https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json"
+    );
+    const jsonData = response.data;
 
-let jsonData = fetch(url)
-  .then((res) => res.json())
-  .catch((err) => {
-    throw err;
-  });
-console.log(jsonData);
-
-app.get("/", (req, res) => {
-  axios
-    .get(url)
-    .then((response) => {
-      const jsonData = response.data;
-      res.json(jsonData.employees);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      res.status(500).send("An error occurred");
-    });
+    // Render the JSON data with the EJS template
+    res.render("index", { jsonData });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-//--------------------------------------------------
+app.get("/", async (req, res) => {
+  try {
+    // Retrieve the JSON data
+    const response = await axios.get(
+      "https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json"
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("An error occurred");
+  }
+});
 
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
